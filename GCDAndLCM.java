49fd4564a3775b94c5ce712d3de63880d2241e8b
Chirag <chirag1678.be21@chitkara.edu.in>
2025-03-07 15:14:45 +0530

    [Chirag] ADD: Find the GCD and LCM of two numbers

---
+++ GCDAndLCM.java
@@ -0,0 +1,55 @@
+// Program to find the Greatest Common Divisor (GCD) and Least Common Multiple (LCM) of two numbers
+import java.util.Scanner;;
+
+class GCDAndLCM {
+    // Main method
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int number1 = input.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int number2 = input.nextInt();
+
+        // method to calculate the greatest common divisor
+        int gcd = calculateGCD(number1, number2);
+
+        // method to calculate the least common multiple
+        int lcm = calculateLCM(number1, number2, gcd);
+
+        System.out.println("GCD of " + number1 + " and " + number2 + " is: " + gcd);
+        System.out.println("LCM of " + number1 + " and " + number2 + " is: " + lcm);
+
+        input.close();
+    }
+
+    // Method to calculate the greatest common divisor
+    public static int calculateGCD(int number1, int number2) {
+        if(number2 == 0) {
+            return number1;
+        }
+
+        return calculateGCD(number2, number1 % number2);
+    }
+
+    // Method to calculate the least common multiple
+    public static int calculateLCM(int number1, int number2, int gcd) {
+        return (number1 * number2) / gcd;
+    }
+}
+// Sample Input ->
+// Enter the first number: 12
+// Enter the second number: 18
+
+// Sample Output ->
+// GCD of 12 and 18 is: 6
+// LCM of 12 and 18 is: 36
+
+// Sample Input ->
+// Enter the first number: 15
+// Enter the second number: 25
+
+// Sample Output ->
+// GCD of 15 and 25 is: 5
+// LCM of 15 and 25 is: 75
