17076852b02022ac0c7660f6a65abbe9c91160ae
Chirag <chirag1678.be21@chitkara.edu.in>
2025-03-07 15:09:19 +0530

    [Chirag] ADD: Calculate factorial of a number

---
+++ FactorialRecursion.java
@@ -0,0 +1,52 @@
+// Program to calculate and print factorial of a number using recursion
+import java.util.Scanner;
+
+class FactorialRecursion {
+    // Main method
+    public static void main(String[] args) {
+        // Method to take number input from user
+        int number = getNumber();
+
+        // Method to calculate factorial of a number using recursion
+        int result = factorial(number);
+
+        // Method to print the factorial of a number
+        printFactorial(number, result);
+    }
+
+    // Method to take number input from user
+    public static int getNumber() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        input.close();
+        return number;
+    }
+
+    // Method to calculate factorial of a number using recursion
+    public static int factorial(int number) {
+        if(number == 0) {
+            return 1;
+        }
+
+        return number * factorial(number - 1);
+    }
+
+    // Method to print the factorial of a number
+    public static void printFactorial(int number, int result) {
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
+// Sample Input ->
+// Enter a number: 5
+
+// Sample Output ->
+// Factorial of 5 is: 120
+
+// Sample Input ->
+// Enter a number: 10
+
+// Sample Output ->
+// Factorial of 10 is: 3628800
